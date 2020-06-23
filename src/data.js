421ce0b7005780e799934dcc2f189c95cb033634
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2020-06-23 18:47:42 +0100

    commit

---
+++ src/data.js
@@ -0,0 +1,86 @@
+import Background1 from './images/image-1.jpg'
+import Background2 from './images/image-2.JPG'
+import Background3 from './images/image-3.JPG'
+import Background4 from './images/image-4.JPG'
+import Background5 from './images/image-5.JPG'
+import Background6 from './images/image-6.JPG'
+import Background7 from './images/image-7.JPG'
+import Background8 from './images/image-8.JPG'
+import Background9 from './images/image-9.JPG'
+import Background10 from './images/image-10.JPG'
+import Background11 from './images/image-11.JPG'
+import Background12 from './images/image-12.JPG'
+
+
+export default [
+  {
+    css:
+      `url(${Background1})`,
+    height: 300
+  },
+  {
+    css:
+    `url(${Background2})`,
+    height: 300
+  },
+  {
+    css:
+    `url(${Background3})`,
+    height: 300
+  },
+  {
+    css:
+    `url(${Background4})`,
+    height: 300
+  },
+  {
+    css:
+    `url(${Background5})`,
+    height: 300
+  },
+  {
+    css:
+    `url(${Background6})`,
+    height: 300
+  },
+  {
+    css:
+    `url(${Background7})`,
+    height: 300
+  },
+  {
+    css:
+    `url(${Background8})`,
+    height: 300
+  },
+  {
+    css:
+    `url(${Background9})`,
+    height: 300
+  },
+  {
+    css:
+    `url(${Background10})`,
+    height: 300
+  },
+  {
+    css:
+    `url(${Background11})`,
+    height: 300
+  },
+  {
+    css:
+    `url(${Background12})`,
+    height: 300
+  },
+  // {
+  //   css:
+  //     'url(https://images.pexels.com/photos/310452/pexels-photo-310452.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)',
+  //   height: 400
+  // },
+  // {
+  //   css:
+  //     'url(https://images.pexels.com/photos/380337/pexels-photo-380337.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)',
+  //   height: 200
+  // }
+]
